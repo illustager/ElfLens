f88eb78746b2eb816ed9b3f8644dcb2a1e6dcb05
illustager <illustager@outlook.com>
2026-06-18 02:49:03 +0800

    fix: TextBox in ScrollViewer for disassembly output

--- src/ElfLens/Views/DisassemblyPanelView.axaml.cs
+++ src/ElfLens/Views/DisassemblyPanelView.axaml.cs
@@ -1,6 +1,4 @@
-using System;
 using Avalonia.Controls;
-using Avalonia.Threading;
 
 namespace ElfLens.Views;
 
@@ -9,19 +7,5 @@ public partial class DisassemblyPanelView : UserControl
     public DisassemblyPanelView()
     {
         InitializeComponent();
-    }
-
-    protected override void OnDataContextChanged(EventArgs e)
-    {
-        base.OnDataContextChanged(e);
-        if (DataContext is Core.ViewModels.DisassemblyPanelViewModel vm)
-        {
-            vm.PropertyChanged += (s, e) =>
-            {
-                if (e.PropertyName == nameof(vm.OutputText))
-                    Dispatcher.UIThread.Post(() => Editor.Text = vm.OutputText);
-            };
-            Dispatcher.UIThread.Post(() => Editor.Text = vm.OutputText);
-        }
     }
 }
