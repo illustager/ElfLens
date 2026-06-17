cfb48fdb76422f2b4f044545cda2516702f4aaae
illustager <illustager@outlook.com>
2026-06-18 06:03:27 +0800

    fix: use code-behind Click handlers for toggle/remove buttons
    Tag stores BreakpointEntry reference for event handler

--- src/ElfLens/Views/BreakpointPanelView.axaml.cs
+++ src/ElfLens/Views/BreakpointPanelView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ElfLens.Core.ViewModels;
 
 namespace ElfLens.Views;
 
@@ -7,5 +9,23 @@ public partial class BreakpointPanelView : UserControl
     public BreakpointPanelView()
     {
         InitializeComponent();
+    }
+
+    private void OnToggleClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.Tag is BreakpointEntry entry &&
+            DataContext is BreakpointPanelViewModel vm)
+        {
+            vm.ToggleCommand.Execute(entry);
+        }
+    }
+
+    private void OnRemoveClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.Tag is BreakpointEntry entry &&
+            DataContext is BreakpointPanelViewModel vm)
+        {
+            vm.RemoveCommand.Execute(entry);
+        }
     }
 }
